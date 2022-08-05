d1dc56e74d58e7de46fda0fa65c51e3f76da74ba
Sebastian <foerster@forestsoft.de>
2022-08-05 16:53:32 +0200

    Xdebug handling

--- build/app/init/10-configure-php.sh
+++ build/app/init/10-configure-php.sh
@@ -4,3 +4,11 @@ if [ "${SESSION_HANDLER:-""}" != "" ]; then
     echo "session.save_handler = $SESSION_HANDLER" >> /usr/local/etc/php/conf.d/50-forestsoft.ini
     echo 'session.save_path = "${SESSION_SAVE_PATH}"' >> /usr/local/etc/php/conf.d/50-forestsoft.ini
 fi
+
+if [ "${XDEBUG_IP:-""}" != "" ]; then
+  echo "xdebug.client_host=$XDEBUG_IP" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+fi
+
+if [ "${XDEBUG_ENABLED:-""}" != "" ]; then
+  echo "xdebug.client_mode=debug" >> /usr/local/etc/php/conf.d/docker-php-ext-xdebug.ini
+fi
