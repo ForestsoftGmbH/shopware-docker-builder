8c479dbc7535bafde2627374fe24a16d1b6f3ff0
Sebastian <foerster@forestsoft.de>
2023-02-26 14:51:46 +0100

    quote savepath to prevent issues with special characters

--- build/app/init/10-configure-php.sh
+++ build/app/init/10-configure-php.sh
@@ -20,4 +20,4 @@ fi
 
 write_config "error_reporting" "$PHP_ERR_REPORTING"
 write_config "session.save_handler" "$SESSION_HANDLER"
-write_config "session.save_path" "$SESSION_SAVE_PATH"
+write_config "session.save_path" "\"$SESSION_SAVE_PATH\""
