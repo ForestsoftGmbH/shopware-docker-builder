7ff5ef962d00bf181445069732f799e1803156ad
Sebastian <foerster@forestsoft.de>
2023-11-22 11:17:14 +0100

    turnover to explicit yes

--- build/app/shopware6/boot-container.sh
+++ build/app/shopware6/boot-container.sh
@@ -31,26 +31,26 @@ if [ "${APP_URL:-""}" != "" ] && [ ! -f "/var/www/html/config/jwt/public.pem" ];
   rm -Rf /var/www/html/var/cache/*
   chown -R www-data:www-data /var/www/html/config/jwt/* /var/www/html/var/cache /var/www/html/var/log
 fi
-if [ "${SKIP_UPDATE:-"0"}" != "1" ]; then
+if [ "${DO_UPDATE:-"0"}" != "0" ]; then
     su -s/bin/bash -c "/var/www/html/bin/console system:update:prepare" www-data
     su -s/bin/bash -c "/var/www/html/bin/console system:update:finish" www-data
 fi
-if [ "${SKIP_PLUGIN_INSTALL:-"0"}" != "1" ]; then
+if [ "${DO_PLUGIN_INSTALL:-"0"}" != "0" ]; then
   /var/www/html/bin/installPlugin.sh
 fi
-if [ "${SKIP_PLUGIN_UPDATE:-"0"}" != "1" ]; then
+if [ "${DO_PLUGIN_UPDATE:-"0"}" != "0" ]; then
   su -s/bin/bash -c "/var/www/html/bin/console plugin:update:all" www-data
 fi
-if [ "${SKIP_THEME_COMPILE:-"0"}" != "1" ]; then
+if [ "${DO_THEME_COMPILE:-"0"}" != "0" ]; then
   su -s/bin/bash -c "/var/www/html/bin/console theme:compile" www-data
 fi
-if [ "${SKIP_ASSET_INSTALL:-"0"}" != "1" ]; then
+if [ "${DO_ASSET_INSTALL:-"0"}" != "0" ]; then
   if [ "${SHOPWARE_ENV:-"production"}" == "test" ]; then
      export SHOPWARE_ENV="production" # Make sure bundles will be copied
   fi
   su -s/bin/bash -c "/var/www/html/bin/console assets:install" www-data
 fi
-if [ "${SKIP_INDEXING:-"0"}" != "1" ]; then
+if [ "${DO_INDEXING:-"0"}" != "0" ]; then
   su -s/bin/bash -c "php -d memory_limit=-1 /var/www/html/bin/console dal:refresh:index --skip product.indexer,media.indexer" www-data
 fi
 if [ -f "/tmp/xdebug.ini" ]; then
